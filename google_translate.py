1a19f2c700cbef2b2185e170c7db72bc343546b4
zcj <424531476@qq.com>
2026-05-15 20:59:00 +0800

    feat(translation): 更新Google翻译API客户端和响应格式解析
    将客户端从'webapp'更改为'gtx'，并迁移到新的API端点
    https://translate.googleapis.com/translate_a/single
    
    添加了对新参数格式的支持，包括hl、dt、bd、dj等参数，
    以获取更准确的翻译结果
    
    更新了响应数据解析逻辑以处理新的JSON格式，
    当dj=1时返回JSON对象格式的数据，同时保持对旧格式的兼容性
    
    增强了错误处理机制，提供更详细的异常信息

--- google_translate.py
+++ google_translate.py
@@ -48,7 +48,7 @@ class Googletrans:
         :param kwargs: 传给requests的参数
         :return: 返回翻译结果
         '''
-        client = 'webapp'
+        client = 'gtx'
         tk = get_tk(q)
         self.lock.acquire()
         if self.n_session_use >= 50:
@@ -56,15 +56,28 @@ class Googletrans:
         self.n_session_use += 1
         session = self.__session
         self.lock.release()
-        url_format = r'https://translate.google.cn/translate_a/single?' \
-                     'client={client}&sl={sl}&tl={tl}&dt=t&q={q}&tk={tk}'
+        
+        # 使用更新的API端点和参数格式
+        url_format = r'https://translate.googleapis.com/translate_a/single?' \
+                     'client={client}&sl={sl}&tl={tl}&hl=zh-CN&dt=t&dt=bd&dj=1&source=icon&tk={tk}&q={q}'
         url = url_format.format(client=client, q=parse.quote(q), tk=tk, sl=sl, tl=tl)
+        
         response = session.get(url, **kwargs)
         if response.status_code != 200:
             raise Exception('status_code:%s' % response.status_code, response.text)
-        text_list = json.loads(response.text)[0]
-        return ''.join([text[0] for text in text_list])
-
+        
+        # 解析新的响应格式
+        try:
+            data = json.loads(response.text)
+            # dj=1 参数返回的是JSON对象格式
+            if 'sentences' in data:
+                return ''.join([sentence['trans'] for sentence in data['sentences']])
+            else:
+                # 兼容旧格式
+                text_list = data[0]
+                return ''.join([text[0] for text in text_list])
+        except (json.JSONDecodeError, KeyError, IndexError) as e:
+            raise Exception('Failed to parse translation response: %s' % str(e))
 
 if __name__ == '__main__':
     translate = Googletrans()
